73b7574d2fac3ba80b418002382c4fde3c509ed0
dzherlitsyn <dzherlitsyn@expertoption.com>
2019-07-22 11:26:15 +0300

    ugly but working

--- jsongrep.py
+++ jsongrep.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import sys
 
 try:
     import ujson as json
@@ -15,6 +16,29 @@ class Settings:
     VERBOSITY = 1
 
 
+def process_line(line, grep_str, exclude_str, super_str, time_gt, time_lt, time_eq):
+    data = json.loads(line)
+
+    if super_str and super_str not in line:
+        return
+
+    msg = data[Settings.MESSAGE_FIELD]
+    if grep_str and grep_str not in msg:
+        return None, None
+    if exclude_str and exclude_str in msg:
+        return None, None
+
+    time = data[Settings.TIME_FIELD]
+    if time_gt and time < time_gt:
+        return None, None
+    if time_lt and time > time_lt:
+        return None, None
+    if time_eq and not time.startswith(time_eq):
+        return None, None
+
+    return time, msg
+
+
 def print_log(file_path, grep_str, exclude_str, super_str,
               time_gt, time_lt, time_eq, verbosity):
     with open(file_path) as f:
@@ -25,25 +49,9 @@ def print_log(file_path, grep_str, exclude_str, super_str,
         while line:
             total_lines += 1
             try:
-                data = json.loads(line)
-
-                if super_str and super_str not in line:
+                time, msg = process_line(line, grep_str, exclude_str, super_str, time_gt, time_lt, time_eq)
+                if msg is None:
                     continue
-
-                msg = data[Settings.MESSAGE_FIELD]
-                if grep_str and grep_str not in msg:
-                    continue
-                if exclude_str and exclude_str in msg:
-                    continue
-
-                time = data[Settings.TIME_FIELD]
-                if time_gt and time < time_gt:
-                    continue
-                if time_lt and time > time_lt:
-                    continue
-                if time_eq and not time.startswith(time_eq):
-                    continue
-
                 print('%s %s' % (time, msg))
                 print_lines += 1
             except Exception as e:
@@ -57,9 +65,31 @@ def print_log(file_path, grep_str, exclude_str, super_str,
                   (print_lines, total_lines, error_lines))
 
 
+def process_stdin(grep_str, exclude_str, super_str,
+                  time_gt, time_lt, time_eq, verbosity):
+    total_lines = 0
+    print_lines = 0
+    error_lines = 0
+    for line in sys.stdin:
+        total_lines += 1
+        try:
+            time, msg = process_line(line, grep_str, exclude_str, super_str, time_gt, time_lt, time_eq)
+            if msg is None:
+                continue
+            print('%s %s' % (time, msg))
+            print_lines += 1
+        except Exception as e:
+            if verbosity > 1:
+                print(e, e.__class__, line)
+            error_lines += 1
+    if verbosity > 0:
+        print(' --- Filtered %s records (total %s, errors %s) ---' %
+              (print_lines, total_lines, error_lines))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", help="File to parse as json log")
+    parser.add_argument("file", help="File to parse as json log", nargs='?')
     parser.add_argument("-s", "--string", help="String to apply as filter to message", nargs='?')
     parser.add_argument("-xs", "--exclude_string", help="String to apply as exclude filter to message", nargs='?')
     parser.add_argument("-ss", "--super_string", help="String to apply as filter to whole json record", nargs='?')
@@ -69,6 +99,11 @@ if __name__ == '__main__':
     parser.add_argument("-v", "--verbosity", type=int, choices=[0, 1, 2], default=Settings.VERBOSITY,
                         help="Increase output verbosity", nargs='?')
     args = parser.parse_args()
-    print_log(args.file, args.string, args.exclude_string, args.super_string,
-              time_gt=args.time_gt, time_lt=args.time_lt, time_eq=args.time_eq,
-              verbosity=args.verbosity)
+    if args.file == 'stdin':
+        process_stdin(grep_str=args.string, exclude_str=args.exclude_string, super_str=args.super_string,
+                  time_gt=args.time_gt, time_lt=args.time_lt, time_eq=args.time_eq,
+                  verbosity=args.verbosity)
+    else:
+        print_log(args.file, grep_str=args.string, exclude_str=args.exclude_string, super_str=args.super_string,
+                  time_gt=args.time_gt, time_lt=args.time_lt, time_eq=args.time_eq,
+                  verbosity=args.verbosity)
